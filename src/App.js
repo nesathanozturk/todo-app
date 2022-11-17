2c3fd8aa2122c2e6ea0d8cb4163a0083494f6a33
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-11-18 00:33:50 +0300

    added react icons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { AiFillDelete } from "react-icons/ai";
+import { AiFillCheckCircle } from "react-icons/ai";
 
 function App() {
   const [newItem, setNewItem] = useState("");
@@ -24,19 +26,24 @@ function App() {
   }
   };
 
-  const completed = id => {
+  const clearItems = () => {
+    setList(list.filter(item => item.completed && !item.completed));
+  };
+
+  const isCompleted = (id) => {
     setList(list.map
       (liEl => liEl.id === id ? { ...liEl, completed : !liEl.completed } : liEl)
       )
   };
 
-  const clearItems = () => {
-    setList(list.filter(item => item.completed && !item.completed));
+  const removeItem = (id) => {
+    const newItems = list.filter((item) => item.id !== id)
+    setList(newItems)
   };
 
   return (
     <div className="App m-0 p-0 h-screen list-none font-poppins">
-      <div className="container bg-white w-2/4 mt-6 m-auto p-4 flex justify-center items-center flex-col rounded-lg">
+      <div className="container bg-white w-2/4 mt-6 m-auto p-5 flex justify-center items-center flex-col rounded-lg">
         <h1 className='text-4xl text-purple-500 font-bold tracking-widest'>My Todo List</h1>
         <form onSubmit={onFormSubmit}>
           <input 
@@ -44,10 +51,10 @@ function App() {
           value={newItem}
           placeholder="Add something to do..."
           onChange={e => setNewItem(e.target.value)}
-          className='w-72 p-[0.5rem] mr-2 mt-5 border-2 border-gray-200 rounded-lg placeholder:tracking-widest placeholder:text-lg focus:outline-none focus:text-gray-800 focus:text-md' 
+          className='w-72 p-[0.4rem] mr-2 mt-5 border-2 border-gray-200 rounded-lg placeholder:tracking-widest placeholder:text-sm focus:outline-none focus:text-gray-800 focus:text-md' 
           />
-          <button className='w-20 bg-purple-700 text-white mr-2 p-[0.45rem] text-xl rounded-md hover:bg-purple-900' onClick={() => addItem(newItem)}>Add</button>
-          <button className='w-20 bg-purple-700 text-white p-[0.45rem] text-xl rounded-md hover:bg-purple-900 transition:color' onClick={() => clearItems()}>Clear</button>
+          <button className='w-20 bg-purple-700 text-white mr-2 p-[0.45rem] text-base rounded-md hover:bg-purple-900' onClick={() => addItem(newItem)}>Add</button>
+          <button className='w-20 bg-purple-700 text-white p-[0.45rem] text-base rounded-md hover:bg-purple-900 transition-color' onClick={() => clearItems()}>Clear</button>
           </form>
       </div>
       <div className="list-container">
@@ -56,10 +63,11 @@ function App() {
             return (
               <li 
               key={item.id} 
-              onClick={() => completed(item.id)}
               className={item.completed ? "done" : ""} 
               >
                 {item.name}
+                <button className="mr-1 text-red-600" onClick={() => removeItem(item.id)}><AiFillDelete /></button>
+                <button className="mr-1 text-green-500" onClick={() => isCompleted(item.id)}><AiFillCheckCircle /></button>
              </li>
             )
           })
