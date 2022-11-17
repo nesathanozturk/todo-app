bb72f417e49c9db7e952ee56b77b6d9175cffd76
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-11-17 11:56:34 +0300

    created main container

--- src/App.js
+++ src/App.js
@@ -3,6 +3,18 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className="container">
+        <h1>React Todo App</h1>
+        <form>
+          <input 
+          type="text" 
+          className='' 
+          onChange=""
+          />
+          <button>Add</button>
+          <button>Clear All</button>
+        </form>
+      </div>
     </div>
   );
 }
