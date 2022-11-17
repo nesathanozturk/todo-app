b8c8e4e7b040664b958dad3e04e043d777e1bd13
Neşathan Öztürk <macbookpro@MacBook-Pro.local>
2022-11-17 13:35:47 +0300

    edited container

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="container">
-        <h1>React Todo App</h1>
-        <form>
+    <div className="App m-0 p-0 box-border h-screen flex justify-center font-roboto text-white">
+      <div className="container w-96 h-96 mt-6">
+        <h1 className='text-4xl tracking-widest'>React Todo App</h1>
           <input 
           type="text" 
-          className='' 
+          placeholder="Add something to do"
+          className='w-72 p-[0.62rem] border-0 rounded-lg mt-5 focus:outline-none placeholder:text-xl focus:text-black focus:font-bold focus:text-md tracking-wide text-are' 
           onChange=""
           />
-          <button>Add</button>
-          <button>Clear All</button>
-        </form>
+          <button className='w-20 bg-purple-700 mx-1 text-xl rounded-md p-[0.5rem]'>Add</button>
       </div>
     </div>
   );
